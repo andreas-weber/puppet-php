faa966c5e39f79e3080dc77ab10978707f0cf816
Andreas Weber <code@andreas-weber.me>
2015-01-19 15:53:40 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+#Puppet Module - PHP
+This module manages php and the installed modules.
+
+##Dependencies
+
+- puppetlabs/stdlib *4.3.2*
+- puppetlabs/apt v1.6.0
+
+## Puppetfile
+
+```
+mod 'aw_php', :git => 'https://github.com/andreas-weber/puppet-php.git'
+```
+
+## Usage
+
+See puppet manifests.
---
+++ manifests/init.pp
@@ -0,0 +1,122 @@
+# == Class: aw_php
+#
+# This module manages php and the installed modules.
+#
+# === Parameters
+#
+# [*development*]
+#   If the development setup should be installed.
+#
+# [*custom_config*]
+#   Path to a custom config.
+#
+# [*remove_apache*]
+#   If the apache webserver should be removed.
+#
+# === Examples
+#
+#   include aw_php
+#
+#   class { 'aw_php':
+#     'development'   => true
+#     'custom_config' => 'aw_php/override.ini.erb'
+#     'remove_apache' => true
+#   }
+#
+# === Authors
+#
+# Andreas Weber <code@andreas-weber.me>
+#
+# === Copyright
+#
+# Copyright 2015 Andreas Weber
+#
+class aw_php(
+  $development = false,
+  $custom_config = 'aw_php/custom.ini.erb',
+  $remove_apache = true
+)
+{
+  class { 'apt':
+    always_apt_update => true
+  }
+
+  apt::key { 'dotdeb-key':
+    key        => '89DF5277',
+    key_source => 'http://www.dotdeb.org/dotdeb.gpg',
+  }
+
+  apt::source { 'dotdeb-php':
+    location          => 'http://packages.dotdeb.org',
+    repos             => 'all',
+    include_src       => true,
+    require           => Apt::Key['dotdeb-key']
+  }
+
+  package {
+    [
+      'php5-cli',
+      'php5-mcrypt',
+      'php5-intl',
+      'php5-mysql',
+      'php5-curl',
+      'php5-xdebug'
+    ]:
+      ensure  => 'latest',
+      require => Apt::Source['dotdeb-php']
+  }
+
+  file { '/etc/php5/conf.d/95-custom.ini':
+    ensure  => file,
+    backup  => false,
+    content => template('aw_php/custom.ini.erb'),
+    require => Package['php5-cli']
+  }
+
+  if($development) {
+    package { 'php5-xdebug':
+      ensure  => 'latest',
+      require => Apt::Source['dotdeb-php']
+    }
+
+    file { '/etc/php5/conf.d/99-development.ini':
+      ensure  => file,
+      backup  => false,
+      content => template('aw_php/development.ini.erb'),
+      require => Package['php5-cli']
+    }
+
+    file { '/etc/php5/conf.d/99-xdebug.ini':
+      ensure  => file,
+      backup  => false,
+      content => template('aw_php/xdebug.ini.erb'),
+      require => Package['php5-xdebug']
+    }
+
+    file { '/usr/bin/php-debug':
+      ensure  => file,
+      backup  => false,
+      content => template('aw_php/php-debug.erb'),
+      mode    => 0777,
+      require => Package['php5-xdebug']
+    }
+
+    file { '/etc/profile.d/php-debug-alias.sh':
+      ensure  => file,
+      backup  => false,
+      content => template('aw_php/php-debug-alias.sh.erb'),
+      mode    => 0644,
+      require => Package['php5-xdebug']
+    }
+  }
+
+  if($remove_apache) {
+    package {
+      [
+        'apache2.2-common'
+      ]:
+        ensure  => 'purged',
+        require => Package['php5-cli']
+    }
+  }
+}
